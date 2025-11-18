34a1101b7267b4d8bcdf2e98c1671f8e7c75592e
Fedde <feddevangils@gmail.com>
2025-11-18 12:30:31 +0100

    Update api.php
    Removed routes for fetching all cases and a specific case.

--- routes/api.php
+++ routes/api.php
@@ -8,11 +8,3 @@ use Illuminate\Support\Facades\Route;
 //     return $request->user();
 // })->middleware('auth:sanctum');
 
-Route::get('/cases', function() {
-    return Investigation::with(['investigator', 'people', 'witnesses'])->get();
-});
-
-// maak een route die specifiek één case ophaalt
-Route::get('/cases/{case}', function($id) {
-    return Investigation::with(['investigator', 'people', 'witnesses'])->find($id);
-});
